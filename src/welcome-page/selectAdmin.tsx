142d9ef8e0d92ccae42a5e5bba098e38917308f2
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-03-07 12:39:01 +0530

    url added to policy admin and travel buddies

--- src/welcome-page/selectAdmin.tsx
+++ src/welcome-page/selectAdmin.tsx
@@ -2,47 +2,69 @@ import { Link } from "react-router-dom";
 import Card from "../common/Cards/card";
 
 const SelectAdmin = () => {
-
   return (
     <div className="main-container page-content">
       <div className="header">
-          <img
-            style={{
-              height: "52px",
-              width: "200px",
-              marginTop: "20px",
-              cursor: "pointer",
-
-            }}
-            src="/assets/BecknLogo.svg"
-            alt={"BecknLogoIcon"}
-          />
+        <img
+          style={{
+            height: "52px",
+            width: "200px",
+            marginTop: "20px",
+            cursor: "pointer",
+          }}
+          src="/assets/BecknLogo.svg"
+          alt={"BecknLogoIcon"}
+        />
       </div>
       <div className="content-wrapper">
-        <div style={{display:"flex", justifyContent:"center", marginTop:"100px"}}>
+        <div className="admin-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "100px",
+          }}
+        >
           <div>
+            <Link
+              to="https://policy-admin.becknprotocol.io/"
+              target="_blank"
+              style={{ textDecoration: "none", color: "#000", width: "100%" }}
+            >
               <Card
                 mainIconUrlInBlack="/assets/policyAdminHover.svg"
                 className="hover_card"
                 mainIconUrl="/assets/policyAdmin.svg"
                 MainTitle={"Policy Admin"}
               />
+            </Link>
           </div>
           <div>
-              <Card
-                mainIconUrlInBlack="/assets/TravelBuddyAdminHover.svg"
-                className="hover_card"
-                mainIconUrl="/assets/TravelBuddyAdmin.svg"
-                MainTitle={"Policy Admin"}
-              />
+            <Link
+              to="https://travelbuddy.becknprotocol.io/Policies"
+              target="_blank"
+              style={{ textDecoration: "none", color: "#000", width: "100%" }}
+            >
+            <Card
+              mainIconUrlInBlack="/assets/TravelBuddyAdminHover.svg"
+              className="hover_card"
+              mainIconUrl="/assets/TravelBuddyAdmin.svg"
+              MainTitle={"Policy Admin"}
+            />
+            </Link>
           </div>
           <div>
+            <Link
+              to="https://travelbuddy.becknprotocol.io/"
+              target="_blank"
+              style={{ textDecoration: "none", color: "#000", width: "100%" }}
+            >
               <Card
                 mainIconUrlInBlack="/assets/travelBuddyApplicationHover.svg"
                 className="hover_card"
                 mainIconUrl="/assets/travelBuddyApplication.svg"
                 MainTitle={"Policy Admin"}
               />
+            </Link>
           </div>
         </div>
       </div>
