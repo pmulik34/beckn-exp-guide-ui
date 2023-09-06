31729fd2d30cfc011c51549cad0388efa1deeab1
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-09-06 15:28:48 +0530

    changinig the retail URL in the selection page

--- src/common/MobileBAP/SelectionPages/selectionPages.tsx
+++ src/common/MobileBAP/SelectionPages/selectionPages.tsx
@@ -23,7 +23,7 @@ const SelectionPages = () => {
       </div>
       <div className="selectionPage_wrapper_innr">
         <a
-          href={`https://retail-app-staging-infra.becknprotocol.io??external_url=${orderObjectUrl}`}
+          href={`https://retail-app-dev.becknprotocol.io/?lang=en&external_url=${orderObjectUrl}`}
           target=""
           rel="noopener noreferrer"
         >
@@ -36,7 +36,7 @@ const SelectionPages = () => {
           </div>
         </a>
         <a
-          href={`https://mobility-app-staging-infra.becknprotocol.io??external_url=${orderObjectUrl}`}
+          href={`https://mobility-app-staging-infra.becknprotocol.io?external_url=${orderObjectUrl}`}
           target=""
           rel="noopener noreferrer"
         >
