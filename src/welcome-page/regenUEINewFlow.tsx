91e40b06680ff2188fd77e9f2bc7bbc5a6c77c45
kundan singh <kundansingh@kundans-MacBook-Pro.local>
2024-02-18 18:01:28 +0530

    created new UEI-flow and update changes

--- src/welcome-page/regenUEINewFlow.tsx
+++ src/welcome-page/regenUEINewFlow.tsx
@@ -8,8 +8,6 @@ const RegenUEINewFlow = () => {
   const sheruAppUrl = process.env.REACT_APP_SHERU_APP_URL;
   const turnoAppUrl = process.env.REACT_APP_TURNO_APP_URL;
   const pulseEnergyVideoUrl = process.env.REACT_APP_PULSE_ENERGY_VIDEO_URL;
-  const sheruVideoUrl = process.env.REACT_APP_SERU_VIDEO_URL;
-  const ternoVideoUrl = process.env.REACT_APP_TURNO_VIDEO_URL;
   const [pulseEnergy, setPulseEnergy] = useState("");
   const [activeUrl, setActiveUrl] = useState(sheruAppUrl);
   const [activeVideoUrl, setActiveVideoUrl] = useState(pulseEnergyVideoUrl);
@@ -18,7 +16,6 @@ const RegenUEINewFlow = () => {
   const handleSheruApp = (e: any) => {
     setActiveButton("Sheru App");
     setActiveUrl(sheruAppUrl);
-    setActiveVideoUrl(sheruVideoUrl);
     setPulseEnergy("");
   };
   const handlePulseEnergy = (e: any) => {
@@ -28,7 +25,6 @@ const RegenUEINewFlow = () => {
   const handleTurnoApp = (e: any) => {
     setActiveButton("Turno app");
     setActiveUrl(turnoAppUrl);
-    setActiveVideoUrl(ternoVideoUrl);
     setPulseEnergy("");
   };
   useEffect(() => {
@@ -44,90 +40,110 @@ const RegenUEINewFlow = () => {
             : "UEI_NewFlow_container_innr"
         }
       >
-        {activeButton === "Pulse energy" ? (
-          <div className="regenUEI_flow_QRcode_new_flow_wrrp">
-            <div className="QR-wrapper regenUEI_flow_QRcode_new_flow">
-              <div className="QR_custom QR_custom_pulse_energy">
-                <QRCode
-                  className="QR_code_pulse_energy regenUEI_flow_QR"
-                  value={pulseEnergyWhatsappUrl || "_"}
-                />
-                <p style={{ paddingBottom: "15px" }}>Or</p>
-                <a
-                  href={pulseEnergyWhatsappUrl || "_"}
-                  target={"/"}
-                  style={{ cursor: "pointer" }}
-                >
-                  <img src="/assets/whatsappImg.svg" />
-                </a>
-              </div>
-            </div>
-            <div className="UEI_newFlow_right_section">
-              <div className="UEI_text">
-                welcome to the unified energy interface
-              </div>
-              <div className="regenUEI_flow_btn UEI_newFlow_btn">
-                <div
-                  onClick={handlePulseEnergy}
-                  className={activeButton === "Pulse energy" ? "active" : ""}
-                >
-                  Pulse energy
-                </div>
-                <div onClick={handleSheruApp}>Sheru App</div>
-                <div onClick={handleTurnoApp}>Turno app</div>
-              </div>
-              <VideoTemplate mainIconUrl={activeVideoUrl} />
-            </div>
+        <div className="beckn_logo_UEI">
+          <img src="/assets/becknImageNew.svg" alt="beckn logo" />
+          <div className="UEI_text">
+            welcome to the unified energy interface
           </div>
-        ) : (
-          <div className="UEI_newFlow">
-            <div className="smartphone-wrapper">
-              <div className="smartphone">
-                <div className="content">
-                  <iframe
-                    //@ts-ignore
-                    className="ChooseExpIframe"
-                    allow="clipboard-read; clipboard-write; geolocation"
-                    src={activeUrl}
-                    frameBorder="0"
-                    allowFullScreen
-                    scrolling={"no"}
-                    width={"100%"}
-                    height={"100%"}
-                    style={{ borderRadius: "36px" }}
+        </div>
+        <div className="UEI_newFlow">
+          {activeButton === "Pulse energy" ? (
+            <div className="regenUEI_flow_QRcode_new_flow_wrrp">
+              <div className="QR-wrapper regenUEI_flow_QRcode_new_flow">
+                <div className="QR_custom QR_custom_pulse_energy">
+                  <QRCode
+                    className="QR_code_pulse_energy regenUEI_flow_QR"
+                    value={pulseEnergyWhatsappUrl || "_"}
                   />
+                  <p style={{ paddingBottom: "15px" }}>Or</p>
+                  <a
+                    href={pulseEnergyWhatsappUrl || "_"}
+                    target={"/"}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <img src="/assets/whatsappImg.svg" />
+                  </a>
                 </div>
               </div>
             </div>
-
-            <div className="UEI_newFlow_right_section">
-              <div className="UEI_text">
-                welcome to the unified energy interface
-              </div>
-              <div className="regenUEI_flow_btn UEI_newFlow_btn">
-                <div
-                  onClick={handlePulseEnergy}
-                  className={activeButton === "Pulse energy" ? "active" : ""}
-                >
-                  Pulse energy
-                </div>
-                <div
-                  onClick={handleSheruApp}
-                  className={activeButton === "Sheru App" ? "active" : ""}
-                >
-                  Sheru App
-                </div>
-                <div
-                  onClick={handleTurnoApp}
-                  className={activeButton === "Turno app" ? "active" : ""}
-                >
-                  Turno app
+          ) : (
+            <div>
+              <div className="smartphone-wrapper">
+                <div className="smartphone">
+                  <div className="content">
+                    <iframe
+                      //@ts-ignore
+                      className="ChooseExpIframe"
+                      allow="clipboard-read; clipboard-write; geolocation"
+                      src={activeUrl}
+                      frameBorder="0"
+                      allowFullScreen
+                      scrolling={"no"}
+                      width={"100%"}
+                      height={"100%"}
+                      style={{ borderRadius: "36px" }}
+                    />
+                  </div>
                 </div>
               </div>
-              <VideoTemplate mainIconUrl={activeVideoUrl} />
+            </div>
+          )}
+          <div className="UEI_newFlow_right_section">
+            <div className="regenUEI_flow_btn UEI_newFlow_btn">
+              <div
+                onClick={handlePulseEnergy}
+                className={
+                  activeButton === "Pulse energy"
+                    ? "active UEI_new_btn"
+                    : "UEI_new_btn"
+                }
+              >
+                Pulse energy
+              </div>
+              <div
+                onClick={handleSheruApp}
+                className={
+                  activeButton === "Sheru App"
+                    ? "active UEI_new_btn"
+                    : "UEI_new_btn"
+                }
+              >
+                Sheru App
+              </div>
+              <div
+                onClick={handleTurnoApp}
+                className={
+                  activeButton === "Turno app"
+                    ? "active UEI_new_btn"
+                    : "UEI_new_btn"
+                }
+              >
+                Turno app
+              </div>
+            </div>
+            <VideoTemplate mainIconUrl={activeVideoUrl} />
+            <div className="UEI_footer_new">
+              <div className="UEI_footer_icons">
+                <img src="/assets/UEI_Icon_Img.svg" />
+              </div>
+              <div className="UEI_footer_icons">
+                <img src="/assets/pulseIconImage.svg" />
+                Pulse Energy
+              </div>
+              <div className="UEI_footer_icons">
+                <img src="/assets/sheruIconImg.svg" />
+              </div>
+              <div className="UEI_footer_icons">
+                <img src="/assets/kazamIconImg.svg" />
+                Kazam
+              </div>
+              <div className="UEI_footer_icons">
+                <img src="/assets/turnoIconImg.svg" />
+                Turno
+              </div>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
